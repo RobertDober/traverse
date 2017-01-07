749f660186dbfe874e45895fe1d7ba72fa4aa26c
RobertDober <robert.dober@gmail.com>
2017-01-07 19:04:27 +0100

    travis with 1.4, version bumped to 0.1.3 for future release

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Traverse.Mixfile do
 
   def project do
     [app: :traverse,
-     version: "0.1.2",
-     elixir: "~> 1.3",
+     version: "0.1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_paths: elixirc_paths(Mix.env),
